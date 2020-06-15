6122ee794eda82175c1515f0e58058fddc50d082
StephC <stephaniebchiang@gmail.com>
2020-06-15 15:32:35 -0400

    update: basic fade

--- client/components/bannerGreeting.js
+++ client/components/bannerGreeting.js
@@ -4,10 +4,10 @@ import {Link} from 'react-router-dom'
 export const BannerGreeting = () => {
   return (
     <div>
-      <p>
+      <p id="bannerG1">
         <Link to="/home">see what i'm working on</Link>
       </p>
-      <p>
+      <p id="bannerG2">
         <Link to="/contact">or contact me here</Link>
       </p>
     </div>
