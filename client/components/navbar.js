5db4c04194d1fbc71900d52bc11955e0f7c7a90a
StephC <stephaniebchiang@gmail.com>
2020-07-14 16:23:43 -0400

    overhaul: scroll and contact state

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,41 +1,55 @@
 import React from 'react'
-import {Link, Switch, Route} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import {Contact} from './index'
 
 export class Navbar extends React.Component {
   constructor() {
     super()
-    // this.state = {
-    //   ifClicked: false
-    // }
+    this.state = {
+      ifClicked: false
+    }
   }
 
   render() {
-    // console.log(this.state)
-    return (
-      <div className="navBar">
-        <nav>
-          <div id="navLink1">
-            {/* <button type="button"></button> */}
-            <Link to="/home/portfolio">PORTFOLIO</Link>
-          </div>
-          <div id="navLink2">
-            <Link to="/home/coding">CODING</Link>
-          </div>
-          <div id="navLink3">
-            <Link to="/home/resume">RESUME</Link>
-          </div>
-          <div id="navLink4">
-            <Link to="/home/contact">CONTACT</Link>
-          </div>
-          <Switch>
-            <Route path="/home/contact" component={Contact} />
-          </Switch>
-          {/* <div id="navLink4">
-            <Link to="/home/fun">JUST FOR FUN</Link>
-          </div> */}
-        </nav>
-      </div>
-    )
+    if (this.state.ifClicked) {
+      return (
+        <div className="navBar">
+          <nav>
+            <div id="navLink1">
+              <Link to="/home/portfolio">PORTFOLIO</Link>
+            </div>
+            <div id="navLink2">
+              <Link to="/home/coding">CODING</Link>
+            </div>
+            <div id="navLink3">
+              <Link to="/home/resume">RESUME</Link>
+            </div>
+            <div id="navLink4">
+              <a onClick={() => this.setState({ifClicked: true})}>CONTACT</a>
+              <Contact />
+            </div>
+          </nav>
+        </div>
+      )
+    } else {
+      return (
+        <div className="navBar">
+          <nav>
+            <div id="navLink1">
+              <Link to="/home/portfolio">PORTFOLIO</Link>
+            </div>
+            <div id="navLink2">
+              <Link to="/home/coding">CODING</Link>
+            </div>
+            <div id="navLink3">
+              <Link to="/home/resume">RESUME</Link>
+            </div>
+            <div id="navLink4">
+              <a onClick={() => this.setState({ifClicked: true})}>CONTACT</a>
+            </div>
+          </nav>
+        </div>
+      )
+    }
   }
 }
